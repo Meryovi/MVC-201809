3045974185401e1d537a1e4bfb0037b220a20a72
Meryovi De Dios <mjdios2406@gmail.com>
2018-09-12 21:07:48 -0400

    Clase del 12-09.

---
+++ src/VentasMvc/VentasMvc/Controllers/ArticulosController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VentasMvc.Modelos;
+
+namespace VentasMvc.Controllers
+{
+    public class ArticulosController : Controller
+    {
+        // GET: Articulos
+        public ActionResult Index()
+        {
+            var context = new VentasMvc.Modelos.VentasEntities();
+            var articulos = context.Articulos.ToList();
+
+            return View(articulos);
+        }
+    }
+}
---
+++ src/VentasMvc/VentasMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VentasMvc.MvcApplication" Language="C#" %>
