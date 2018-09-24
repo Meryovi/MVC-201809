ba6bcfdcc87b9f0b9d54420c4a31074373915da7
Meryovi De Dios <mjdios2406@gmail.com>
2018-09-24 10:50:43 -0400

    Correccion campo de cantidad, que no viajaba al Controller.

--- src/VentasMvc/VentasMvc/Models/FacturasAgregarArticuloModel.cs
+++ src/VentasMvc/VentasMvc/Models/FacturasAgregarArticuloModel.cs
@@ -12,7 +12,7 @@ namespace VentasMvc.Models
         [Display(Name = "Articulo")]
         public int ArticuloId { get; set; }
 
-        public int Cantidad { get; internal set; }
+        public int Cantidad { get; set; }
 
         public SelectList ListaArticulos { get; set; }
     }
